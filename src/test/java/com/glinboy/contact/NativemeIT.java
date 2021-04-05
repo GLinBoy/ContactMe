ac5fceb3e695fe7d23cccb65c79940561c62a700
GLinBoy <hojjatabedie@gmail.com>
2021-04-05 22:55:08 +0430

    🕯 START: Project initiation and setup structure

---
+++ src/test/java/com/glinboy/contact/NativemeIT.java
@@ -0,0 +1,9 @@
+package com.glinboy.contact;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativemeIT extends meTest {
+
+    // Execute the same tests but in native mode.
+}
